63cf2ed9a84299c66de323a419b6ca318968c905
Edson Silva <edson.s@lyncas.net>
2024-01-08 18:35:34 -0300

    Implementado um Timeout Interceptor para que o client nao fique eternamente esperando uma resposta caso alguma das APIs caia

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as momentTimezone from 'moment-timezone'; // https://momentjs.com/timezone/docs/
 import { AllExceptionsFilter } from './filters/http-exception.filter';
+import { LoggingInterceptor } from './interceptors/logging.interceptor'
+import { TimeoutInterceptor } from './interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new LoggingInterceptor(), new TimeoutInterceptor());
   app.useGlobalFilters(new AllExceptionsFilter());
 
   Date.prototype.toJSON = function (): any {
