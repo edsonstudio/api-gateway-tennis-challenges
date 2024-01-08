951b1271a48f9fc84702a0951d70dacec93b06de
Edson Silva <edson.s@lyncas.net>
2024-01-08 13:36:29 -0300

    Criado o filtro global para tratar as exceptions, aplicadas as logicas do filtro global de exceptions e o coversor de datas para o timezone pt-br no arquivo main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as momentTimezone from 'moment-timezone'; // https://momentjs.com/timezone/docs/
+import { AllExceptionsFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  Date.prototype.toJSON = function (): any {
+    return momentTimezone(this).tz('America/Sao_Paulo').format('YYYY-MM-DD HH:mm:ss.SSS');
+  };
+
   await app.listen(3000);
 }
 bootstrap();
