894fc36e8281b617aecc37f47a2ead04fe53ea6c
Edson Silva <edson.s@lyncas.net>
2024-01-08 15:08:22 -0300

    Criado o primeiro metodo da API-Gateway que sera o endpoint que adicionara na fila a requisicao para criar uma categoria

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Logger, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices';
+import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
 
-@Controller()
+@Controller('api/v1')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  private logger = new Logger(AppController.name);
+  private clientAdminBackend: ClientProxy;
+
+  constructor() {
+    this.clientAdminBackend = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqps://qgskholi:ZHnzxLfW3KglP05tiKWMKSJ2JXAdiKcV@prawn.rmq.cloudamqp.com/qgskholi'],
+        queue: 'admin-backend'
+      }
+    });
   }
+
+  @Post('categorias')
+  @UsePipes(ValidationPipe)
+  async criarCategoria(@Body() criarCategoriaDto: CriarCategoriaDto) {
+    return await this.clientAdminBackend.emit('criar-categoria', criarCategoriaDto);
+  }
+
 }
